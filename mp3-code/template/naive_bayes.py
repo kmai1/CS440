8ea90cceacdf02a95808f53eccf8c7fd520ec7ed
Kevin Mai <maikevin123@gmail.com>
2020-10-07 03:57:17 -0500

    unigram complete

--- mp3-code/template/naive_bayes.py
+++ mp3-code/template/naive_bayes.py
@@ -7,6 +7,7 @@
 #
 # Created by Justin Lizama (jlizama2@illinois.edu) on 09/28/2018
 import math
+from nltk.corpus import stopwords
 """
 This is the main entry point for MP3. You should only modify code
 within this file and the last two arguments of line 34 in mp3.py
@@ -33,7 +34,8 @@ def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter=1.0, pos_pr
     # TODO: Write your code here
     # return predicted labels of development set
     predicted_labels = []
-    stop_words = ["ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here", "than"]
+    stop_words = ["ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here", "than", "the", "br", "i" ,'just', 'herself', 'm', 'too', 'down', 'same', "shouldn't", 'no', 'because', "doesn't", 'ma', 't', "should've", 'ain', 'here', 'the', 'does', 'you', 'be', 've', 'y', 'mustn', 's', 'again', 'did', 'between', 'these', 'all', 'doesn', 'this', 'can', 'have', 'there', "couldn't", "needn't", "don't", 'shan', 'was', 'while', 'been', 'do', 'from', 'once', 'didn', 'its', 'for', 'is', "aren't", 'theirs', 'it', 'as', 'hadn', 'them', 'to', 'll', 'themselves', 'own', 'being', 'an', 'needn', 'in', 'your', 'yours', 'our', 'hers', 'they', 'but', 'with', 'of', "isn't", 'couldn', 'only', 'now', 'him', 'over', "you'd", 'hasn', "hadn't", "you'll", 'on', 'me', 'more', 'wasn', 'having', 'isn', "hasn't", 'itself', 'his', 'aren', 'and', 'which', 'above', "you're", 'into', 'those', 'd', 'we', 'before', 'both', "wasn't", 'she', 'few', "haven't", 'after', 'myself', 'not', 'so', 'by', 'am', 'most', 'were', 're', "it's", 'each', 'shouldn', 'out', 'than', 'some', 'her', 'are', 'against', 'through', 'when', 'such', 'haven', 'weren', 'had', 'where', 'himself', "she's", "didn't", "wouldn't", 'off', 'other', 'ours', 'then', "shan't", "won't", 'nor', 'mightn', "mustn't", 'during', 'at', 'ourselves', 'if', 'yourself', 'further', 'any', 'who', 'a', 'that', 'or', 'doing', 'o', 'below', 'why', 'wouldn', 'very', "you've", 'has', 'won', 'should', "weren't", 'whom', 'about', 'don', 'how', 'up', 'until', 'will', 'what', 'under', 'he', 'my', 'i', "that'll", 'yourselves', 'their', "mightn't"]
+
     posWords = {}
     negWords = {}
     # these two store every word
@@ -43,13 +45,13 @@ def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter=1.0, pos_pr
     numOfPosWords = 0
     numOfNegWords = 0
     # laplace smoothing bigram_smoothing_parameter
-    alpha = 0.45
+
     #creates the dictionary of frequency for wrods in pos/neg reviews on tset
     for i in range(len(train_labels)):
         if (train_labels[i] == 1):
             for j in range(len(train_set[i])):
-                if (train_set[i][j] in stop_words):
-                    continue
+                # if (train_set[i][j].lower() in stop_words):
+                #     continue
                 totalPosWords += 1
                 working_word = train_set[i][j].lower()
                 if (working_word in posWords):
@@ -58,8 +60,8 @@ def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter=1.0, pos_pr
                     posWords[working_word] = 1
         else:
             for j in range(len(train_set[i])):
-                if (train_set[i][j] in stop_words):
-                    continue
+                # if (train_set[i][j].lower() in stop_words):
+                #     continue
                 totalNegWords += 1
                 working_word = train_set[i][j].lower()
                 if (working_word in negWords):
@@ -84,19 +86,19 @@ def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter=1.0, pos_pr
             if (word in stop_words):
                 continue
             if (word not in posWords):
-                numOfPosWords += 1
+                # numOfPosWords += 1
                 countInPos = 0
-            else :
+            else:
                 countInPos = posWords[word]
 
             if (word not in negWords):
-                numOfNegWords += 1
+                # numOfNegWords += 1
                 countInNeg = 0
-            else :
+            else:
                 countInNeg = negWords[word]
             # V + 1 = numOfPos/NegWords bc its already updated, no need to account for
-            pos_probability = math.log((countInPos + alpha) / (totalPosWords + (alpha * numOfPosWords)))
-            neg_probability = math.log((countInNeg + alpha) / (totalNegWords + (alpha * numOfNegWords)))
+            pos_probability = math.log((countInPos + smoothing_parameter) / (totalPosWords + (smoothing_parameter * (numOfPosWords + 1))))
+            neg_probability = math.log((countInNeg + smoothing_parameter) / (totalNegWords + (smoothing_parameter * (numOfNegWords + 1))))
 
             update_pos_prior += pos_probability
             update_neg_prior += neg_probability
@@ -105,7 +107,7 @@ def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter=1.0, pos_pr
             predicted_labels.append(1)
         else:
             predicted_labels.append(0)
-    print(alpha)
+    print(smoothing_parameter)
     return predicted_labels
 
 def bigramBayes(train_set, train_labels, dev_set, unigram_smoothing_parameter=1.0, bigram_smoothing_parameter=1.0, bigram_lambda=0.5,pos_prior=0.8):
@@ -128,5 +130,5 @@ def bigramBayes(train_set, train_labels, dev_set, unigram_smoothing_parameter=1.
     """
     # TODO: Write your code here
     # return predicted labels of development set using a bigram model
-
-    return naive_bayes(train_set, train_labels, dev_set, smoothing_parameter=1.0, pos_prior=0.8)
+    
+    return []
